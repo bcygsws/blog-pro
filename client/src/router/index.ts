b69ac4a2c6dbc1c49467112bf925536a1e836e56
bcygsws <bcygswspring168@gmail.com>
2024-08-19 05:48:40 +0800

    :memo: 路由文件代码简化

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -4,52 +4,44 @@ import {getToken} from "@/utils/token";
 const routes = [
     {
         path: '/',
-        redirect: '/home',
-        children: [// 一级组件，两个/home和/login
+        component: () => import("@/views/home/HomeView.vue"),
+        redirect: '/main',
+        children: [
             {
-                path: '/home',
-                component: () => import("@/views/home/HomeView.vue"),
-                redirect: '/main',
-                children: [
-                    {
-                        path: '/main',
-                        component: () => import("@/components/main/MainView.vue")
-                    },
-                    {
-                        path: '/category/:id',
-                        component: () => import("@/components/category/CategoryView.vue")
-                    }
-                ]
+                path: '/main',
+                component: () => import("@/components/main/MainView.vue")
             },
             {
-                path: '/login',
-                component: () => import("@/views/login/Login.vue")
-            },
-            {
-                path: '/dashboard',
-                component: () => import("@/views/dashboard/DashBoard.vue"),
-                redirect: '/dashboard/manage_art',
-                children: [
-                    {
-                        path: '/dashboard/manage_art',
-                        component: () => import("@/components/manage/ManageArt.vue")
-
-                    },
-                    {
-                        path: '/dashboard/manage_cat',
-                        component: () => import("@/components/manage/ManageCat.vue")
-                    }
-                ]
-            },
-            {
-                path: '/detail/:id',
-                component: () => import("@/components/detail/Detail.vue")
-
+                path: '/category/:id',
+                component: () => import("@/components/category/CategoryView.vue")
             }
-
         ]
     },
+    {
+        path: '/login',
+        component: () => import("@/views/login/Login.vue")
+    },
+    {
+        path: '/dashboard',
+        component: () => import("@/views/dashboard/DashBoard.vue"),
+        redirect: '/dashboard/manage_art',
+        children: [
+            {
+                path: '/dashboard/manage_art',
+                component: () => import("@/components/manage/ManageArt.vue")
 
+            },
+            {
+                path: '/dashboard/manage_cat',
+                component: () => import("@/components/manage/ManageCat.vue")
+            }
+        ]
+    },
+    {
+        path: '/detail/:id',
+        component: () => import("@/components/detail/Detail.vue")
+
+    }
 ];
 const router = createRouter({
     history: createWebHistory(),
